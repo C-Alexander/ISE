ae90abd775e2b7ab2e1706fea8cdebbe324c63be
C-Alexander <alexander@contritio.com>
2015-01-25 04:18:07 +0100

    The girl disappeared while watching bebop - so Worlds Most Ridiculously Small Feature came. Stars. Yes, real stars. Not like Kanye West, more like the type astronauts see.
    Oh, yes, I am using a for loop instead of the built in wrap method. Mostly because honestly, I don't want to give it origin variables and all again. I am going to put off that stuff while I can.
    
    Edit: I'd like to note this is the peak of commit messages this project is going to get. From this point on the quality of the commit messages will only degrade. Though of course ill promise myself, once again, not to get lazy with them, I naturally will do just that.

--- core/src/com/contritio/ise/InterStellarEmpires.java
+++ core/src/com/contritio/ise/InterStellarEmpires.java
@@ -5,23 +5,32 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 
 public class InterStellarEmpires extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture backGround;
-	
+	private Vector2 backGroundSize;
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		backGround = new Texture("legacy/stars.jpg");
+		backGroundSize = new Vector2(backGround.getWidth(), backGround.getHeight()); //Yes, I know there's a wrap method with GL. But that means needing a second batch etc.
+		//thisl do.
+
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(0, 0, 0, 150);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		Gdx.gl.glClearColor(0, 0, 0, 0);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); //I cant freaking believe this still can not be transparent. I know WebGL supports it.
+		//But I am not freaking forking LibGDX just to change that. Especially not because they're even more stubborn than I am. They refuse to let me pull request if I dont use Eclipse...
 		batch.begin();
-		batch.draw(backGround, 0, 0);
+		for (int x = 0; x < Gdx.graphics.getWidth(); x += backGroundSize.x) {
+			for (int y = 0; y < Gdx.graphics.getHeight(); y += backGroundSize.y) {
+				batch.draw(backGround, x, y);
+			}
+		}
 		batch.end();
 	}
 }
